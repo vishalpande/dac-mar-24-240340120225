28d37b39c1ae69e002c93870ae4c76861f94f368
vishalpande <pandevishal40@gmail.com>
2024-05-28 15:10:07 +0530

    b1

--- Hello.java
+++ Hello.java
@@ -1,10 +1,12 @@
 import java.util.*;
 class Hello{
 public static void main(String[] args) {
-    System.err.println("Hello World!");
-System.out.println("heyy Buddy");
+//main    
+System.out.println("Hello World!");
+
+
+System.out.println("B1");
+
 
 }
-
-
 }
